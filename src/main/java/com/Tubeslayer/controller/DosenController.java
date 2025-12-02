6d94e3c1b5218a841bf740083b0077e4777142d4
Marsella Moretta <6182301058@student.unpar.ac.id>
2025-12-03 00:40:01 +0700

    Added changes

--- src/main/java/com/Tubeslayer/controller/DosenController.java
+++ src/main/java/com/Tubeslayer/controller/DosenController.java
@@ -10,4 +10,24 @@ public class DosenController {
     public String dosenDashboard() {
         return "dosen/dashboard";  // templates/dosen/dashboard.html
     }
+
+        // 1. Mapping untuk halaman daftar semua mata kuliah
+    @GetMapping("/dosen/mata-kuliah")
+    public String mahasiswaMataKuliahList() {
+        return "dosen/mata-kuliah";
+    }
+
+    // 2. Mapping untuk halaman detail mata kuliah (Kuliah)
+    @GetMapping("/dosen/matkul-detail") 
+    public String mahasiswaMatkulDetail() {
+        // template location: templates/Matakuliah/matkul-detail.html
+        return "dosen/matkul-detail"; 
+    }
+
+    // 3. Mapping untuk halaman daftar peserta
+    // Template: templates/Matakuliah/matkul-peserta.html
+    @GetMapping("/dosen/matkul-peserta")
+    public String mahasiswaMatkulPeserta() {
+        return "dosen/matkul-peserta"; 
+    }
 }
