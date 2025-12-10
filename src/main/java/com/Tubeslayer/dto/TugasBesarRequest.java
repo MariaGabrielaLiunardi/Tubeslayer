926e58a7ffa71dd00f898b98446ea2cacd0ec955
Marsella Moretta <6182301058@student.unpar.ac.id>
2025-12-10 14:37:40 +0700

    Fix bug in pemilihan anggota

--- src/main/java/com/Tubeslayer/dto/TugasBesarRequest.java
+++ src/main/java/com/Tubeslayer/dto/TugasBesarRequest.java
@@ -7,6 +7,10 @@ public class TugasBesarRequest {
     private String judulTugas;
     private String deadline; // <--- Diterima sebagai String (YYYY-MM-DD)
     private String deskripsi;
-    
-   
+    private String modeKel;
+
+    public void setModeKel(String modeKel) { 
+        this.modeKel = modeKel;
+    }
+
 }
