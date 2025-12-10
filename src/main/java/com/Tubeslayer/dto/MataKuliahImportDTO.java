031e7a8e84be3e13dc617bf36436df3fd9a7aa37
clarajoy-png <6182301053@student.unpar.ac.id>
2025-12-10 16:44:09 +0700

    Update Profil

---
+++ src/main/java/com/Tubeslayer/dto/MataKuliahImportDTO.java
@@ -0,0 +1,11 @@
+package com.Tubeslayer.dto;
+
+import lombok.Data;
+
+@Data
+public class MataKuliahImportDTO {
+    private String kode;
+    private String nama;
+    private Integer sks;
+    private String status;
+}
