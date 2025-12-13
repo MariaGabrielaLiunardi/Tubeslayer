0d2fffda8df0dd37e6c8bfb307793836bccfb48d
Maria Gabriela Liunardi <6182301012@student.unpar.ac.id>
2025-12-13 12:25:55 +0700

    nilai dosen pt sekian

--- src/main/java/com/Tubeslayer/repository/NilaiRepository.java
+++ src/main/java/com/Tubeslayer/repository/NilaiRepository.java
@@ -6,7 +6,36 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface NilaiRepository  extends JpaRepository<Nilai, Integer>{
+public interface NilaiRepository extends JpaRepository<Nilai, Integer>{
     
+    /**
+     * Cari nilai berdasarkan user dan tugas
+     */
+    Optional<Nilai> findByUser_IdUserAndTugas_IdTugas(String idUser, Integer idTugas);
+    
+    /**
+     * Cari semua nilai untuk satu tugas
+     */
+    List<Nilai> findByTugas_IdTugas(Integer idTugas);
+    
+    /**
+     * Cari semua nilai untuk satu user
+     */
+    List<Nilai> findByUser_IdUser(String idUser);
+    
+    /**
+     * Check apakah nilai sudah ada untuk user dan tugas tertentu
+     */
+    boolean existsByUser_IdUserAndTugas_IdTugas(String idUser, Integer idTugas);
+    
+    /**
+     * Cari nilai untuk user dalam suatu kelompok dan tugas
+     */
+    @Query("SELECT n FROM Nilai n " +
+           "WHERE n.tugas.idTugas = :idTugas " +
+           "AND n.user.idUser IN (SELECT uk.user.idUser FROM UserKelompok uk WHERE uk.kelompok.idKelompok = :idKelompok)")
+    List<Nilai> findByTugasAndKelompok(@Param("idTugas") Integer idTugas, @Param("idKelompok") Integer idKelompok);
 }
+
